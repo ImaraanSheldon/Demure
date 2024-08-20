f9e01748cf3d0eac26ea33477601796da8a6b229
mohammad-mohamed <mohammad.mhm16@gmail.com>
2024-08-20 12:15:45 +0200

    all components made and added a contactview for now

---
+++ src/components/Footer.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container-fluid" id="full-footer">
+        <img src="https://iili.io/JGNgqF4.png" alt="nav-logo">
+        <div class="container links">
+            <p>Quick Links</p>
+            <router-link to="/" id="link">Home</router-link>
+            <router-link to="/about" id="link">About</router-link>
+            <router-link to="/products" id="link">Store</router-link>
+            <router-link to="/admin" id="link">Admin</router-link>
+            <router-link to="/contact" id="link">Contact Us</router-link>
+        </div>
+        <div class="container" id="contact-info">
+            <h4>contact us</h4>
+            <div class="socials">
+                <span>
+                    Mohammad Mohamed
+                </span>
+                <span class="icons">
+                    <a href="https://wa.me/0833858267" target="_blank">
+                        <i class="fa fa-whatsapp"></i>
+                    </a>
+                    <a href="https://www.instagram.com/mohammad.mhm16/" target="_blank">
+                        <i class="fa fa-instagram"></i>
+                    </a>
+                    <a href="mohammad.mhm16@gmail.com" target="_blank">
+                        <i class="fa-regular fa-envelope"></i>
+                    </a>
+                </span>
+            </div>
+            <div class="socials">
+                <span>
+                    Imaraan Sheldon
+                </span>
+                <span class="icons">
+                    <a href="https://wa.me/" target="_blank">
+                        <i class="fa fa-whatsapp"></i></a>
+                    <a href="https://www.instagram.com/imraan/" target="_blank"><i
+                            class="fa fa-instagram"></i></a>
+                    <a href="imaraansheldonis2302003@gmail.com" target="_blank"><i
+                            class="fa-regular fa-envelope"></i></a>
+                </span>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'FooterComp'
+}
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,35 @@
+<template>
+    <button class="btn menubtn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling"
+        aria-controls="offcanvasScrolling"><i class="fa-solid fa-bars"></i>
+    </button>
+
+    <div class="offcanvas offcanvas-start" data-bs-scroll="true" data-bs-backdrop="true" tabindex="-1"
+        id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+        <div class="offcanvas-header">
+            <router-link to="/" id="nav-logo">
+                <img src="" alt="logo">
+            </router-link>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            <ul id="list">
+                <li><router-link to="/" id="nav-item">Home</router-link></li>
+                <li><router-link to="/about" id="nav-item">About</router-link></li>
+                <li><router-link to="/products" id="nav-item">Store</router-link></li>
+                <li><router-link to="/admin" id="nav-item">Admin</router-link></li>
+                <li><router-link to="/contact" id="nav-item">Contact Us</router-link></li>
+            </ul>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'NavBarComp'
+}
+</script>
+
+<style>
+
+
+
+</style>
