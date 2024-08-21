f8d275e87c0a69bbd2ca5803230a9f13073b1890
mohammad-mohamed <mohammad.mhm16@gmail.com>
2024-08-21 15:01:09 +0200

    made a for now navbar, will update it later on

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,60 +1,100 @@
 <template>
-    <nav class="navbar navbar-danger bg-danger bg-opacity-125 sticky-top">
-      <div class="container-fluid justify-content-between">
-        <img
-          src="https://mohammad-mohamed.github.io/hostedImages/images/JS_EOMP/Icon/falafel_icon.jpg"
-          alt="Logo"
-          class="navbar-brand"
-        />
-        <div class="collapse" id="navbarToggleExternalContent" data-bs-theme="dark">
-          <nav class="navbar navbar-dark bg-danger">
-            <div class="container-fluid">
-              <ul class="navbar-nav d-flex flex-row">
-                <li class="nav-item">
-                  <router-link class="nav-link me-5" to="/">Home</router-link>
-                </li>
-                <li class="nav-item">
-                  <router-link class="nav-link me-5" to="/about">About Us</router-link>
-                </li>
-                <li class="nav-item">
-                  <router-link class="nav-link me-5" to="/products">Product</router-link>
-                </li>
-                <li class="nav-item">
-                  <router-link class="nav-link me-5" to="/admin">Admin</router-link>
-                </li>
-                <li class="nav-item">
-                  <router-link class="nav-link me-5" to="/contact">Contact</router-link>
-                </li>
-              </ul>
-            </div>
-          </nav>
+  <div class="holder">
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg bg-brown">
+      <div class="container-fluid">
+        <!-- Toggler and category -->
+        <div class="d-flex align-items-center">
+          <button type="button" class="btn navbar-show-btn text-white me-2">
+            <i class="fas fa-bars"></i>
+          </button>
+
+          <!-- Side navbar -->
+          <ul id="side-navbar" class="bg-white text-uppercase list-unstyled d-none">
+            <button type="button" class="btn navbar-hide-btn text-dark">
+              <i class="fas fa-times"></i>
+            </button>
+            <li><router-link to="/" class="nav-link">home</router-link></li>
+            <li><router-link to="/about" class="nav-link">about</router-link></li>
+            <li><router-link to="/shop" class="nav-link">shop</router-link></li>
+            <li><router-link to="/admin" class="nav-link">admin</router-link></li>
+            <li><router-link to="/contact" class="nav-link">contact</router-link></li>
+          </ul>
+          <!-- end of side navbar -->
         </div>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarToggleExternalContent"
-          aria-controls="navbarToggleExternalContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
+
+        <!-- Main navigation list -->
+        <div class="collapse navbar-collapse justify-content-end">
+          <!-- nav list -->
+          <ul class="navbar-nav text-uppercase mb-2 mb-lg-0">
+            <li class="nav-item">
+              <router-link to="/" class="nav-link active-link">
+                <span class="nav-link-text">home</span>
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/about" class="nav-link">
+                <span class="nav-link-text">about</span>
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/shop" class="nav-link">
+                <span class="nav-link-text">shop</span>
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/admin" class="nav-link">
+                <span class="nav-link-text">admin</span>
+              </router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/contact" class="nav-link">
+                <span class="nav-link-text">contact</span>
+              </router-link>
+            </li>
+          </ul>
+          <!-- end of nav list -->
+
+          <!-- Account icons -->
+          <div class="d-flex">
+            <router-link to="/profile" class="btn text-white">
+              <i class="fas fa-person"></i>
+            </router-link>
+            <router-link to="/cart" class="btn text-white ms-2">
+              <i class="fas fa-shopping-basket"></i>
+            </router-link>
+          </div>
+          <!-- end of account icons -->
+        </div>
+        <!-- end of main navigation list -->
       </div>
     </nav>
-  </template>
-  
-  <script>
-  export default {};
-  </script>
-  
-  <style scoped>
-  .navbar-brand {
-    width: 60px;
+    <!-- end of navbar -->
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    const navShowBtn = document.querySelector('.navbar-show-btn');
+    const navHideBtn = document.querySelector('.navbar-hide-btn');
+    const sideNavbar = document.getElementById('side-navbar');
+
+    // Show the sidebar
+    navShowBtn.addEventListener('click', () => {
+      sideNavbar.classList.remove('d-none');
+      sideNavbar.classList.add('d-block');
+    });
+
+    // Hide the sidebar
+    navHideBtn.addEventListener('click', () => {
+      sideNavbar.classList.remove('d-block');
+      sideNavbar.classList.add('d-none');
+    });
   }
-  
-  .nav-link {
-    margin-right: 1.25rem; /* equivalent to me-5 in Bootstrap */
-  }
-  </style>
-  
+};
+</script>
+
+<style scoped>
+
+</style>
