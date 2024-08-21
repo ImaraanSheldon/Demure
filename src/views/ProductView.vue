8ed929bce1edc94a5fec8327f11d9d53a1c04c02
mohammad-mohamed <mohammad.mhm16@gmail.com>
2024-08-21 16:32:52 +0200

    made the products/product view for now and changed

--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,13 +1,34 @@
 <template>
-    <div>
-        
+    <div class="container-fluid" id="single-view">
+        <div class=" sing-img">
+            <img :src="product.prodUrl" alt="prod-png" id="prod-png" loading="lazy">
+        </div>
+        <div class="prod-bio">
+            <img :src="product.prodBio" alt="product-bio" id="prod-bg" loading="lazy">
+            <div class="container prod-details">
+                <h2>ABOUT</h2>
+                <p>name: {{ product.prodName }}</p>
+                <p>Price: R{{ product.amount }}</p>
+                <p class="desc">About: {{ product.prodDes }}</p>
+                <button class="cart">
+                    add to cart
+                </button>
+            </div>
+        </div>
     </div>
 </template>
 <script>
+
 export default {
-    
+    components: {
+    },
+    computed: {
+        product() {
+            return this.$store.state.product;
+        }
+    },
+    mounted() {
+        this.$store.dispatch('fetchProduct', this.$route.params)
+    }
 }
 </script>
-<style>
-    
-</style>
--- src/views/ProductsView.vue
+++ src/views/ProductsView.vue
@@ -1,13 +1,76 @@
 <template>
-    <div>
-        
+    <div class="container-fluid" id="Products">
+        <div class="search container">
+            <input type="text" name="search" v-model="search" @input="searchProd()" id="searchBar" placeholder="Name/Category">
+            <button id="sort" type="button" @click="sortPrice()">Sort By Price</button>
+        </div>
+        <div class="row" v-if="products" id="prod-cards">
+            <card v-for="product in searchProd() || sortPrice()" :key="product.prodID">
+                <template #prod-img>
+                    <img class="prod-img" :src="product.prodUrl" loading="lazy">
+                </template>
+                <template #prod-title>
+                    {{ product.prodName }}
+                </template>
+                <template #Desc>
+                    <p>
+                        Category: {{ product.category }}
+                    </p>
+                    <p>
+                        Amount: R{{ product.amount }}
+                    </p>
+                    <p>
+                        Quantity: {{ product.quantity }}
+                    </p>
+                    <router-link :to="{ name: 'product', params: { id: product.prodID } }" id="see-more">View
+                        More</router-link>
+                </template>
+            </card>
+        </div>
+        <div class="row" v-else>
+            <div class="lead">
+                <spinner />
+            </div>
+        </div>
     </div>
 </template>
+
 <script>
+import card from '@/components/Card.vue';
+import spinner from '@/components/Spinner.vue';
 export default {
-    
+    components: {
+        card,
+        spinner
+    },
+    data() {
+        return {
+            search: ''
+        }
+    },
+    methods: {
+        searchProd() {
+            let furniture = this.$store.state.products;
+            let find = this.search;
+            let found = furniture.filter(prod => {
+                return prod.prodName.toLowerCase().includes(find.toLowerCase()) || prod.category.toLowerCase().includes(find.toLowerCase());
+            });
+            return found
+        },
+        sortPrice() {
+            let unsorted = this.$store.state.products
+            if (unsorted){
+                unsorted.sort((a, b) => a.amount - b.amount)
+            }
+        }
+    },
+    computed: {
+        products() {
+            return this.$store.state.products
+        }
+    },
+    mounted() {
+        this.$store.dispatch('fetchProducts')
+    }
 }
 </script>
-<style>
-    
-</style>
